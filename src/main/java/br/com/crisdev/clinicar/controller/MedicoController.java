d9d44fd2b7510edd249c3be57e482869fdb6fb6a
CristianeAlves <crisalves_ef@yahoo.com.br>
2023-12-05 19:09:54 -0300

    Listar medicos

--- src/main/java/br/com/crisdev/clinicar/controller/MedicoController.java
+++ src/main/java/br/com/crisdev/clinicar/controller/MedicoController.java
@@ -1,17 +1,14 @@
 package br.com.crisdev.clinicar.controller;
 
-import br.com.crisdev.clinicar.domain.medico.DadosCadastroMedico;
-import br.com.crisdev.clinicar.domain.medico.DadosDetalhamentoMedico;
-import br.com.crisdev.clinicar.domain.medico.Medico;
-import br.com.crisdev.clinicar.domain.medico.MedicoRepository;
+import br.com.crisdev.clinicar.domain.medico.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -28,5 +25,11 @@ public class MedicoController {
         repository.save(medico);
         var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
         return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(page);
     }
 }
---
+++ src/main/java/br/com/crisdev/clinicar/domain/medico/DadosListagemMedico.java
@@ -0,0 +1,14 @@
+package br.com.crisdev.clinicar.domain.medico;
+
+public record DadosListagemMedico(
+        Long id,
+        String nome,
+        String email,
+        String crm,
+        Especialidade Especialidade
+) {
+
+    public DadosListagemMedico(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
