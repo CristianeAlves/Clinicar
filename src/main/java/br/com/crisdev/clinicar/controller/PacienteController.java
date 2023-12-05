7fd4f00f7b4039df06ef463c44c5d0c95d875381
CristianeAlves <crisalves_ef@yahoo.com.br>
2023-12-05 20:29:40 -0300

    Cadastrar paciente

---
+++ src/main/java/br/com/crisdev/clinicar/controller/PacienteController.java
@@ -0,0 +1,33 @@
+package br.com.crisdev.clinicar.controller;
+
+import br.com.crisdev.clinicar.domain.paciente.DadosCadastroPaciente;
+import br.com.crisdev.clinicar.domain.paciente.DadosDetalhamentoPaciente;
+import br.com.crisdev.clinicar.domain.paciente.Paciente;
+import br.com.crisdev.clinicar.domain.paciente.PacienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+    @Autowired
+    PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriBuilder) {
+        var paciente = new Paciente(dados);
+        repository.save(paciente);
+
+        var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
+    }
+}
---
+++ src/main/java/br/com/crisdev/clinicar/domain/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,16 @@
+package br.com.crisdev.clinicar.domain.paciente;
+
+import br.com.crisdev.clinicar.domain.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(
+        Long id,
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        Endereco endereco
+) {
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
