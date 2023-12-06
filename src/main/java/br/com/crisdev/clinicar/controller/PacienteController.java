b70389697e102c389da11b465427a1fe227d4f17
CristianeAlves <crisalves_ef@yahoo.com.br>
2023-12-05 21:22:12 -0300

    Listar pacientes

--- src/main/java/br/com/crisdev/clinicar/controller/PacienteController.java
+++ src/main/java/br/com/crisdev/clinicar/controller/PacienteController.java
@@ -1,17 +1,14 @@
 package br.com.crisdev.clinicar.controller;
 
-import br.com.crisdev.clinicar.domain.paciente.DadosCadastroPaciente;
-import br.com.crisdev.clinicar.domain.paciente.DadosDetalhamentoPaciente;
-import br.com.crisdev.clinicar.domain.paciente.Paciente;
-import br.com.crisdev.clinicar.domain.paciente.PacienteRepository;
+import br.com.crisdev.clinicar.domain.paciente.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -29,5 +26,11 @@ public class PacienteController {
 
         var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
         return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemPaciente>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+        return ResponseEntity.ok().body(page);
     }
 }
---
+++ src/main/java/br/com/crisdev/clinicar/domain/paciente/DadosListagemPaciente.java
@@ -0,0 +1,12 @@
+package br.com.crisdev.clinicar.domain.paciente;
+
+public record DadosListagemPaciente(
+        Long id,
+        String nome,
+        String cpf,
+        String email
+) {
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getCpf(), paciente.getEmail());
+    }
+}
