be13a85710feb5036d0443b64ab30f88dd0a2f0a
CristianeAlves <crisalves_ef@yahoo.com.br>
2023-12-10 16:12:42 -0300

    Autenticacao

--- src/main/java/br/com/crisdev/clinicar/infra/exception/TratadorDeErros.java
+++ src/main/java/br/com/crisdev/clinicar/infra/exception/TratadorDeErros.java
@@ -1,4 +1,4 @@
-package br.com.crisdev.clinicar.infra;
+package br.com.crisdev.clinicar.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
