0fda30d403d1f0259f91296e4ad0268fb47ca8ce
CristianeAlves <crisalves_ef@yahoo.com.br>
2023-12-10 17:10:55 -0300

    Gerar token

--- src/main/java/br/com/crisdev/clinicar/controller/AutenticacaoController.java
+++ src/main/java/br/com/crisdev/clinicar/controller/AutenticacaoController.java
@@ -1,12 +1,14 @@
 package br.com.crisdev.clinicar.controller;
 
 import br.com.crisdev.clinicar.domain.usuario.DadosAutenticacao;
+import br.com.crisdev.clinicar.domain.usuario.Usuario;
+import br.com.crisdev.clinicar.infra.security.DadosTokenJWT;
+import br.com.crisdev.clinicar.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +21,15 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
---
+++ src/main/java/br/com/crisdev/clinicar/infra/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package br.com.crisdev.clinicar.infra.security;
+
+public record DadosTokenJWT(String token) {
+}
