f3050be87409c1ec93657736d60a3efc3ad0bbe5
CristianeAlves <crisalves_ef@yahoo.com.br>
2023-12-15 21:13:49 -0300

    Documentacao

--- src/main/java/br/com/crisdev/clinicar/controller/ConsultaController.java
+++ src/main/java/br/com/crisdev/clinicar/controller/ConsultaController.java
@@ -1,6 +1,7 @@
 package br.com.crisdev.clinicar.controller;
 
 import br.com.crisdev.clinicar.domain.consulta.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
